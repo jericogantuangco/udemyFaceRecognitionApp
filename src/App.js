ec934a2e96d0407324ea1937554463b62e572baf
jericogantuangco <jericogantuangco200@gmail.com>
2020-02-16 19:48:51 +0800

    added based front end view of the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particleOptions = {
+  "particles": {
+    "number": {
+        "value": 50
+    },
+    "size": {
+        "value": 3
+    }
+  },
+  "interactivity": {
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "repulse"
+          }
+      }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles' params={particleOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*
+      
+      <FaceRecognition />*/}
     </div>
   );
 }
